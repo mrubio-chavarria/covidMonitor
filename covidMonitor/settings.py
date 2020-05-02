42bd57d816d6b65dc8d650bcacd5758e8afd1431
mrubio-chavarria <msrch@hotmail.es>
2020-05-02 13:18:54 +0200

    Add app maps

--- covidMonitor/settings.py
+++ covidMonitor/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    'services.maps',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
